2f862e9cc7f87c2d893a158f0e401635ce166f2f
iaron <iaron.araujo@vtex.com.br>
2019-08-19 16:11:47 -0300

    More refactoring to remove unused code

--- react/components/Star.tsx
+++ react/components/Star.tsx
@@ -1,10 +1,9 @@
 import React, { FunctionComponent } from 'react'
 
-const Star: FunctionComponent<StarProps> = ({key, fill, className, size}) => {
+const Star: FunctionComponent<StarProps> = ({fill, className, size}) => {
   return (
     <svg
       className={className}
-      key={key}
       xmlns="http://www.w3.org/2000/svg"
       width={size}
       height={size}
@@ -15,7 +14,7 @@ const Star: FunctionComponent<StarProps> = ({key, fill, className, size}) => {
         d="M7.369,11.251,11.923,14,10.714,8.82l4.023-3.485-5.3-.449L7.369,0,5.3,4.885,0,5.335,4.023,8.82,2.815,14Z"
         transform="translate(0)"
       />
-    </svg> // se o review.metrics.rating for 4, preenche 4 estrelas
+    </svg>
   )
 
 }
--- react/components/Stars.tsx
+++ react/components/Stars.tsx
@@ -5,8 +5,8 @@ import styles from '../styles.css'
 
 const Stars: FunctionComponent<StarsProps> = ({ rating }) => {
   return (
-    <div className={`${styles.review__rating_stars} dib relative v-mid mr2`}>
-      <div className={`${styles.review__rating_inactive} nowrap`}>
+    <div className={`${styles.ratingStars} dib relative v-mid mr2`}>
+      <div className={`${styles.ratingStarsInactive} nowrap`}>
         {[0, 1, 2, 3, 4].map((_, i) => {
           return (
             <Star key={i} fill={'#eee'} className="mr2" size={20} />
@@ -14,7 +14,7 @@ const Stars: FunctionComponent<StarsProps> = ({ rating }) => {
         })}
       </div>
       <div
-        className={`${styles.review__rating_active} nowrap overflow-hidden absolute top-0-s left-0-s`}
+        className={`${styles.ratingStarsActive} nowrap overflow-hidden absolute top-0-s left-0-s`}
         style={{ width: rating * 20 + '%' }}
       >
         {[0, 1, 2, 3, 4].map((_, i) => {
