74c76b2e853e15d597e9520fc774647405bf22b7
iaron <iaron.araujo@vtex.com.br>
2019-09-04 15:15:21 -0300

    Fixed some styling

--- react/components/Stars.tsx
+++ react/components/Stars.tsx
@@ -3,46 +3,48 @@ import Star from './Star'
 
 import styles from '../styles.css'
 
-const getStarPercentage = (rating:number, i:number) => {
-  if(rating >= (i+1)) {
-    return '100%'
+const getStarPercentage = (rating: number, i: number) => {
+  let percentage
+  if (rating >= i + 1) {
+    percentage = '100%'
+  } else if (i < rating && rating < i + 1) {
+    percentage = `${(rating - i) * 100}%`
+  } else {
+    percentage = '0%'
   }
-  return `${((rating - i) * 100)}%`
+  return percentage
+}
+
+const generateStars = (rating: number) => {
+  const stars = []
+  for (let i = 0; i < 5; i++) {
+    stars.push(
+      <div className={`${styles.inactiveStarContainer} relative mr2`}>
+        <Star
+          key={i}
+          fill="currentColor"
+          className={styles.inactiveStar}
+          size={20}
+        />
+        <div
+          className={`${styles.ratingStarsActive} nowrap overflow-hidden absolute top-0-s left-0-s`}
+          style={{ width: getStarPercentage(rating, i) }}
+        >
+          <span key={i} className={styles.activeStarContainer}>
+            <Star fill="currentColor" className={styles.activeStar} size={20} />
+          </span>
+        </div>
+      </div>
+    )
+  }
+  return stars
 }
 
 const Stars: FunctionComponent<StarsProps> = ({ rating }) => {
   return (
     <div className={`${styles.ratingStars} dib relative v-mid mr2`}>
       <div className={`${styles.ratingStarsInactive} nowrap flex`}>
-        {[0, 1, 2, 3, 4].map((_, i) => {
-          return (
-            <div className="relative mr2">
-              <Star
-                key={i}
-                fill="currentColor"
-                className={''}
-                size={20}
-              />
-              <div
-                className={`${styles.ratingStarsActive} nowrap overflow-hidden absolute top-0-s left-0-s`}
-                style={{ width: getStarPercentage(rating, i) }}
-              >
-                <span
-                  key={i}
-                  className={
-                    rating > i ? styles.activeStarColor : styles.inactiveStarColor
-                  }
-                >
-                  <Star
-                    fill="currentColor"
-                    className={i === 4 ? '' : ''}
-                    size={20}
-                  />
-                </span>
-              </div>
-            </div>
-          )
-        })}
+        {generateStars(rating)}
       </div>
     </div>
   )
