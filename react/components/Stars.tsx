d83f670aa3f149b6cd135ac6f16cf2ba40f40367
iaron <iaron.araujo@vtex.com.br>
2019-09-04 15:32:57 -0300

    improved the code

--- react/components/Stars.tsx
+++ react/components/Stars.tsx
@@ -4,47 +4,42 @@ import Star from './Star'
 import styles from '../styles.css'
 
 const getStarPercentage = (rating: number, i: number) => {
-  let percentage
   if (rating >= i + 1) {
-    percentage = '100%'
+    return '100%'
   } else if (i < rating && rating < i + 1) {
-    percentage = `${(rating - i) * 100}%`
+    return `${(rating - i) * 100}%`
   } else {
-    percentage = '0%'
+    return '0%'
   }
-  return percentage
-}
-
-const generateStars = (rating: number) => {
-  const stars = []
-  for (let i = 0; i < 5; i++) {
-    stars.push(
-      <div className={`${styles.inactiveStarContainer} relative mr2`}>
-        <Star
-          key={i}
-          fill="currentColor"
-          className={styles.inactiveStar}
-          size={20}
-        />
-        <div
-          className={`${styles.ratingStarsActive} nowrap overflow-hidden absolute top-0-s left-0-s`}
-          style={{ width: getStarPercentage(rating, i) }}
-        >
-          <span key={i} className={styles.activeStarContainer}>
-            <Star fill="currentColor" className={styles.activeStar} size={20} />
-          </span>
-        </div>
-      </div>
-    )
-  }
-  return stars
 }
 
 const Stars: FunctionComponent<StarsProps> = ({ rating }) => {
+  const fiveStars = [0, 1, 2, 3, 4]
   return (
     <div className={`${styles.ratingStars} dib relative v-mid mr2`}>
       <div className={`${styles.ratingStarsInactive} nowrap flex`}>
-        {generateStars(rating)}
+        {
+          fiveStars.map(i => {
+            return(
+              <div key={i} className={`${styles.inactiveStarContainer} relative mr2`}>
+              <Star
+                key={i}
+                fill="currentColor"
+                className={styles.inactiveStar}
+                size={20}
+              />
+              <div
+                className={`${styles.ratingStarsActive} nowrap overflow-hidden absolute top-0-s left-0-s`}
+                style={{ width: getStarPercentage(rating, i) }}
+              >
+                <span className={styles.activeStarContainer}>
+                  <Star fill="currentColor" className={styles.activeStar} size={20} />
+                </span>
+              </div>
+            </div>
+            )
+          })
+        }
       </div>
     </div>
   )
