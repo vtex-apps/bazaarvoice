e84e19a55a961558d6b30d78c042b0bbd01c5706
iaron <iaron.araujo@vtex.com.br>
2019-09-04 14:17:23 -0300

    Fixed stars

--- react/components/Stars.tsx
+++ react/components/Stars.tsx
@@ -3,39 +3,44 @@ import Star from './Star'
 
 import styles from '../styles.css'
 
+const getStarPercentage = (rating:number, i:number) => {
+  if(rating >= (i+1)) {
+    return '100%'
+  }
+  return `${((rating - i) * 100)}%`
+}
+
 const Stars: FunctionComponent<StarsProps> = ({ rating }) => {
   return (
     <div className={`${styles.ratingStars} dib relative v-mid mr2`}>
-      <div className={`${styles.ratingStarsInactive} nowrap`}>
+      <div className={`${styles.ratingStarsInactive} nowrap flex`}>
         {[0, 1, 2, 3, 4].map((_, i) => {
           return (
-            <Star
-              key={i}
-              fill="currentColor"
-              className={i === 4 ? '' : 'mr2'}
-              size={20}
-            />
-          )
-        })}
-      </div>
-      <div
-        className={`${styles.ratingStarsActive} nowrap overflow-hidden absolute top-0-s left-0-s`}
-        style={{ width: rating * 20 + '%' }}
-      >
-        {[0, 1, 2, 3, 4].map((_, i) => {
-          return (
-            <span
-              key={i}
-              className={
-                rating > i ? styles.activeStarColor : styles.inactiveStarColor
-              }
-            >
+            <div className="relative mr2">
               <Star
+                key={i}
                 fill="currentColor"
-                className={i === 4 ? '' : 'mr2'}
+                className={''}
                 size={20}
               />
-            </span>
+              <div
+                className={`${styles.ratingStarsActive} nowrap overflow-hidden absolute top-0-s left-0-s`}
+                style={{ width: getStarPercentage(rating, i) }}
+              >
+                <span
+                  key={i}
+                  className={
+                    rating > i ? styles.activeStarColor : styles.inactiveStarColor
+                  }
+                >
+                  <Star
+                    fill="currentColor"
+                    className={i === 4 ? '' : ''}
+                    size={20}
+                  />
+                </span>
+              </div>
+            </div>
           )
         })}
       </div>
