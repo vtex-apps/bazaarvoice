8f8cecb479b3a3632e62842e728385197863062d
iaron <iaron.araujo@vtex.com.br>
2019-09-04 15:35:15 -0300

    more fixes

--- react/components/Stars.tsx
+++ react/components/Stars.tsx
@@ -6,22 +6,25 @@ import styles from '../styles.css'
 const getStarPercentage = (rating: number, i: number) => {
   if (rating >= i + 1) {
     return '100%'
-  } else if (i < rating && rating < i + 1) {
-    return `${(rating - i) * 100}%`
-  } else {
-    return '0%'
   }
+  if (i < rating && rating < i + 1) {
+    return `${(rating - i) * 100}%`
+  }
+  return '0%'
 }
 
+const fiveStars = [0, 1, 2, 3, 4]
+
 const Stars: FunctionComponent<StarsProps> = ({ rating }) => {
-  const fiveStars = [0, 1, 2, 3, 4]
   return (
     <div className={`${styles.ratingStars} dib relative v-mid mr2`}>
       <div className={`${styles.ratingStarsInactive} nowrap flex`}>
-        {
-          fiveStars.map(i => {
-            return(
-              <div key={i} className={`${styles.inactiveStarContainer} relative mr2`}>
+        {fiveStars.map(i => {
+          return (
+            <div
+              key={i}
+              className={`${styles.inactiveStarContainer} relative mr2`}
+            >
               <Star
                 key={i}
                 fill="currentColor"
@@ -33,13 +36,16 @@ const Stars: FunctionComponent<StarsProps> = ({ rating }) => {
                 style={{ width: getStarPercentage(rating, i) }}
               >
                 <span className={styles.activeStarContainer}>
-                  <Star fill="currentColor" className={styles.activeStar} size={20} />
+                  <Star
+                    fill="currentColor"
+                    className={styles.activeStar}
+                    size={20}
+                  />
                 </span>
               </div>
             </div>
-            )
-          })
-        }
+          )
+        })}
       </div>
     </div>
   )
