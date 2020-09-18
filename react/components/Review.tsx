b7aa874422606aeaf386a1ed7e38f195d63da5c0
Breno Calazans <brenocalazans@gmail.com>
2020-09-18 14:46:20 -0300

    Add typings and lint file

--- react/components/Review.tsx
+++ react/components/Review.tsx
@@ -59,11 +59,17 @@ const Review: FunctionComponent<ReviewProps> = ({ review, appSettings }) => {
       </h5>
       <div className="flex flex-column-s flex-row-ns">
         <div className="flex flex-grow-1 flex-column w-70-ns">
+          <div className={`${styles.reviewByField} t-small c-muted-1`}>
+            {`${review.UserNickname || '-'} ${
+              review.UserLocation ? `, from ${review.UserLocation}` : ''
+            }`}
+          </div>
+
           {review.Photos.length ? (
             <div
               className={`${styles.reviewImagesContainer} mt6 flex items-start`}
             >
-              {review.Photos.map((item: any, i: number) => {
+              {review.Photos.map((item, i: number) => {
                 return (
                   <img
                     alt="Product"
@@ -75,32 +81,55 @@ const Review: FunctionComponent<ReviewProps> = ({ review, appSettings }) => {
               })}
             </div>
           ) : null}
-          <div className={`${styles.reviewByField} t-small c-muted-1`}>
-            {review.UserNickname} {review.UserLocation && `, from ${review.UserLocation}`}
+
+          <p className={`${styles.reviewText} t-body lh-copy mw7 pr5-ns`}>
+            {review.ReviewText}
+          </p>
+
+          {appSettings.showClientResponses && review.ClientResponses.length ? (
+            <div className={`${styles.clientResponseContainer} mw7 pr5-ns pl7`}>
+              {review.ClientResponses.map(item => {
+                return (
+                  <div
+                    key={item.Date}
+                    className={`${styles.clientResponse} t-body lh-copy`}
+                  >
+                    <div
+                      className={`${styles.clientResponseDepartment} t-body b c-muted-1`}
+                    >
+                      {item.Department}
+                    </div>
+                    <div
+                      className={`${styles.clientResponseMessage} t-body lh-copy`}
+                    >
+                      {item.Response}
+                    </div>
+                    <div
+                      className={`${styles.clientResponseType} t-body c-muted-1`}
+                    >
+                      {item.ResponseType}
+                    </div>
+                    <div
+                      className={`${styles.clientResponseName} t-body c-muted-1`}
+                    >
+                      {item.Name} - {item.ResponseSource}
+                    </div>
+                    <div
+                      className={`${styles.clientResponseDate} t-small c-muted-1`}
+                    >
+                      {getTimeAgo(item.Date)}
+                    </div>
                   </div>
-                  <p className={`${styles.reviewText} t-body lh-copy mw7 pr5-ns`}>
-                      {review.ReviewText}
-                  </p>
-                  {appSettings.showClientResponses && review.ClientResponses.length ? (
-                      <div className={`${styles.clientResponseContainer} mw7 pr5-ns pl7`}>
-                          {review.ClientResponses.map((item: any) => {
-                              return (
-                                  <div className={`${styles.clientResponse} t-body lh-copy`}>
-                                      <div className={`${styles.clientResponseDepartment} t-body b c-muted-1`}>{item.Department}</div>
-                                      <div className={`${styles.clientResponseMessage} t-body lh-copy`}>{item.Response}</div>
-                                      <div className={`${styles.clientResponseType} t-body c-muted-1`}>{item.ResponseType}</div>
-                                      <div className={`${styles.clientResponseName} t-body c-muted-1`}>{item.Name} - {item.ResponseSource}</div>
-                                      <div className={`${styles.clientResponseDate} t-small c-muted-1`}>{getTimeAgo(item.Date)}</div>
-                                  </div>
-                              )
-                          })}
-                      </div>
-                  ) : null
-                  }
+                )
+              })}
+            </div>
+          ) : null}
+
         </div>
+
         {review.SecondaryRatings && (
           <ul className="flex flex-grow-1 flex-column pl0 pl3-ns list">
-            {review.SecondaryRatings.map((rating: any, i: number) => {
+            {review.SecondaryRatings.map((rating, i) => {
               if (rating == null) {
                 return <li key={i} />
               }
@@ -132,8 +161,54 @@ const Review: FunctionComponent<ReviewProps> = ({ review, appSettings }) => {
 }
 
 interface ReviewProps {
-    review: any
-    appSettings: any
+  review: Review
+  appSettings: any
+}
+
+interface Review {
+  Id: string
+  ClientResponses: ClientResponse[]
+  Photos: Photo[]
+  Rating: number
+  ReviewText: string
+  SecondaryRatings: SecondaryRating[]
+  SubmissionTime: string
+  Title: string
+  UserLocation: string
+  UserNickname: string
+}
+
+interface Photo {
+  Sizes: ImageSize
+}
+
+interface ImageSize {
+  normal: Image
+  thumbnail: Image
+}
+
+interface Image {
+  Url: string
+}
+
+interface ClientResponse {
+  Department: string
+  Response: string
+  ResponseType: string
+  ResponseSource: string
+  Name: string
+  Date: string
+}
+
+interface SecondaryRating {
+  DisplayType: string
+  Id: string
+  Label: string
+  MaxLabel: string | null
+  MinLabel: string | null
+  Value: number
+  ValueLabel: string | null
+  ValueRange: number
 }
 
 export default Review
